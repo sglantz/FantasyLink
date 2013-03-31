9b6ebb50b3411f648681db020b1252a984c08e63
Seth Glantz <seth.glantz@gmail.com>
2013-03-31 10:42:08 -0700

    Version 1.0! The extension has been updated to work with both CBSSports and ESPN fantasy baseball.  Links appear on the team and free agent pages on both sites.
    Signed-off-by: Seth Glantz <seth.glantz@gmail.com>

--- contentscript.js
+++ contentscript.js
@@ -4,23 +4,57 @@
  *    to do by hand." ~ Douglas Adams, just zis guy, you know
  */
 
-var timeDelay = 400;
+var timeDelay = 1200;
  
 $(document).ready(function () {
-    generateLinks();
-	if (document.URL.indexOf("/flb/freeagency") != -1) {
-		timeDelay = 1200;
-	}
+    if(document.URL.indexOf("espn") != -1){
+		addESPNResetEvents();
+        addESPNLinks();
+    } else if (document.URL.indexOf("cbssports") != -1){
+		addCBSResetEvents();
+		addCBSLinks();       
+    }
 });
 
-function generateLinks() {
+function addESPNLinks() {
     $('.FanGraphLink').remove();
     $('.playertablePlayerName').children(':first-child').each(function() {
-        var newLink='<a class="FanGraphLink" href="http://www.fangraphs.com/players.aspx?lastname=' + $(this).text() + '" target="_blank"><img src="http://www.fangraphs.com/favicon.ico" height="12" width="12" border="0" style="margin:0 6px 0 6px" title="FanGraphs" /></a>';
-        $(this).parent().append(newLink);
+        $(this).parent().append(getFanGraphLink($(this).text()));
     });
 }
 
-$('#lastNameSubmit, #playerTableHeader, .playertablefiltersmenucontainer, .games-toolset').live('click', function() {
-  setTimeout('generateLinks()', timeDelay);
-});
+function addESPNResetEvents(){
+    if (document.URL.indexOf("/flb/freeagency") == -1) {
+        timeDelay = 400;
+    }
+	$('#lastNameSubmit, #playerTableHeader, .playertablefiltersmenucontainer, .games-toolset').click(function() {
+		setTimeout('addSPNLinks()', timeDelay);
+	});
+}
+
+function addCBSResetEvents(){
+	$('.fantasyTabGroup').find('a').click(function() {
+		setTimeout('generateCBSLinks()', timeDelay);
+	});
+	$('table.filter').find('a').click(function() {
+		setTimeout('addCBSLinks()', timeDelay);
+	});
+}
+
+function addCBSLinks(){
+    $('.FanGraphLink').remove();
+	$('a.playerLink').each(function() {
+		if($(this).parent().is('td')){
+			var splitIndex = $(this).text().indexOf(', ');
+			var originalName = $(this).text()
+			$(this).parent().append(getFanGraphLink(originalName.substring(splitIndex + 2, originalName.length) + ' ' + 
+				originalName.substring(0, splitIndex)));
+		}
+	});
+}
+
+function getFanGraphLink(playerName){
+	return '<a class="FanGraphLink" href="http://www.fangraphs.com/players.aspx?lastname=' + playerName + 
+		'" target="_blank"><img src="http://www.fangraphs.com/favicon.ico" height="12" width="12" border="0"' +
+		' style="margin:0 6px 0 6px" title="FanGraphs" /></a>';
+}
